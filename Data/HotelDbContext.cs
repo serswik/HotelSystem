b2d9e27eb68b8aa455e9d6bd775330b967f96b85
serswik <megasergik@icloud.com>
2024-11-27 18:09:51 +0200

    Initial commit

---
+++ Data/HotelDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelSystem.Models;
+
+namespace HotelSystem.Data
+{
+    public class HotelDbContext : DbContext
+    {
+        public HotelDbContext(DbContextOptions<HotelDbContext> options) : base(options) { }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms {  get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        /* Filling up database info
+         * protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Room>().HasData(
+                )
+        }
+        */ 
+    }
+
+}
