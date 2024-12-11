993fc197e51221f41921252f499c76797d36926a
serswik <megasergik@icloud.com>
2024-12-11 16:47:23 +0200

    Temporari commit, need to fix the options in field Type while editing Room

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -51,5 +51,121 @@ namespace HotelSystem.Controllers
             return View(room);
         }
 
+        // GET: Room/Edit/5
+        public async Task<IActionResult> Edit (int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }    
+
+            var room = await _context.Rooms.FindAsync(id);
+            if(room == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Hotels = await _context.Hotels.ToListAsync();
+            ViewBag.RoomTypes = Enum.GetValues(typeof(RoomType))
+                .Cast<RoomType>()
+                .Select(rt => new SelectListItem { Value = rt.ToString(), Text = rt.ToString() })
+                .ToList();
+
+            return View(room);
+        }
+
+        // POST: Room/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,HotelId,Type,Price,Description,IsAvailable")] Room room)
+        {
+            if (id != room.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(room);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomExists(room.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Hotels = await _context.Hotels.ToListAsync();
+            ViewBag.RoomTypes = Enum.GetValues(typeof(RoomType))
+                .Cast<RoomType>()
+                .Select(rt => new SelectListItem { Value = rt.ToString(), Text = rt.ToString() })
+                .ToList();
+
+            return View(room);
+        }
+
+        // GET: Room/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms
+                .Include(r => r.Hotel)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // GET: Room/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms
+                .Include(r => r.Hotel)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // POST: Room/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var room = await _context.Rooms.FindAsync(id);
+            _context.Rooms.Remove(room);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoomExists(int id)
+        {
+            return _context.Rooms.Any(e => e.Id == id);
+        }
     }
 }
